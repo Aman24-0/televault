6f637a16cbe716347034dadaace7c42397c96f53
Aman Dahayat <dahayataman@gmail.com>
2026-05-10 01:17:19 +0530

    Postgre db update

--- backend/core/database.py
+++ backend/core/database.py
@@ -9,7 +9,14 @@ load_dotenv()
 # Render environment se URL uthayega
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_async_engine(DATABASE_URL, echo=False)
+# database.py mein ye change karein
+engine = create_async_engine(
+    DATABASE_URL, 
+    echo=False,
+    pool_pre_ping=True,  # Isse connection drop nahi hoga
+    pool_size=10, 
+    max_overflow=20
+)
 AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
 async def get_db():
